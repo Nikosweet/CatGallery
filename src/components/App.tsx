181b0f6ade67a7ae1a979ac57c9309b9de2f06ca
Nikosweet <kupchikov1975@mail.ru>
2026-04-09 10:41:38 +0700

    Skeleton

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+import classes from '@/components/App.module.scss'
+import Header from '@/components/Header'
+
+export default function App() {
+  return (
+  <>
+    <Header></Header>
+    <main className={classes.main}><Outlet/></main>
+  </>
+    )
+}
---
+++ src/index.tsx
@@ -0,0 +1,36 @@
+import {createRoot} from 'react-dom/client'
+import App from './components/App'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Children } from 'react';
+import { Shop } from './pages/shop';
+import { About } from '@/pages/about';
+
+const root = document.getElementById('root')
+
+if (!root) {
+    throw new Error('root not found');
+}
+
+const container = createRoot(root);
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/about',
+                element: <About />
+            },
+            {
+                path: '/shop',
+                element: <Shop/>
+            },
+        ]
+    }
+])
+
+container.render(
+    <RouterProvider router={router} />
+);
